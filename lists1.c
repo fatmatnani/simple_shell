a1b2f5b77389ad5898cf75dcc4952590e127bf01
fatma tnani <fatmatnani08@gmail.com>
2023-02-22 12:04:05 -0800

    second commit

--- lists1.c
+++ lists1.c
@@ -26,32 +26,32 @@ size_t list_len(const list_t *h)
  */
 char **list_to_strings(list_t *head)
 {
-list_t *node = head;
-size_t i = list_len(head), j;
-char **strs;
-char *str;
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
 
-if (!head || !i)
-	return (NULL);
-	strs = malloc(sizeof(char *) * (i + 1));
-if (!strs)
-	return (NULL);
-for (i = 0; node; node = node->next, i++)
-{
-	str = malloc(_strlen(node->str) + 1);
-	if (!str)
-	{
-		for (j = 0; j < i; j++)
-			free(strs[j]);
-		free(strs);
+	if (!head || !i)
 		return (NULL);
-	}
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
 
-	str = _strcpy(str, node->str);
-	strs[i] = str;
-}
-strs[i] = NULL;
-return (strs);
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
 }
 
 
